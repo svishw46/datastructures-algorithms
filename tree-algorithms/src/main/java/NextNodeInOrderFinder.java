67e9b82e4f432e283253dcd95db49447dbc89f96
Rangaraju,Vengada Karthik <venkat060890@gmail.com>
2015-04-05 20:33:29 -0500

    NextNodeInOrderFinder

---
+++ tree-algorithms/src/main/java/NextNodeInOrderFinder.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Write an algorithm to find the 'next' node(in-order successor) of a given node in a binary search tree.
+ * You may assume that each node has a link to its parent.
+ */
+public class NextNodeInOrderFinder {
+    private List<Integer> integerList = new ArrayList<Integer>(2);
+
+    public int findSuccessor(Node node, int value) {
+        findSuccessorRecursive(node, value);
+
+        return this.integerList.size() == 2 ? this.integerList.get(1) : -1;
+    }
+
+    public void findSuccessorRecursive(Node node, int value) {
+        if (node == null) {
+            return;
+        }
+
+        //Find in leftSubTree.
+        findSuccessorRecursive(node.left, value);
+
+        //If there is already a value that matched, node.value should be
+        //the successor.
+        if (this.integerList.size() == 1) {
+            this.integerList.add(node.value);
+        }
+
+        //Add the value that matched.
+        if (node.value == value) {
+            this.integerList.add(node.value);
+        }
+
+        findSuccessorRecursive(node.right, value);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node two = new Node(2);
+        Node three = new Node(3);
+        Node four = new Node(4);
+        Node five = new Node(5);
+        Node six = new Node(6);
+        Node seven = new Node(7);
+
+        root.left = two;
+        root.right = three;
+
+        two.left = four;
+        two.right = five;
+
+        three.left = six;
+        three.right = seven;
+
+        RecursiveTreeTraverser recursiveTreeTraverser = new RecursiveTreeTraverser();
+        recursiveTreeTraverser.traverseInOrder(root);
+
+        NextNodeInOrderFinder nextNodeInOrderFinder = new NextNodeInOrderFinder();
+        int successor = nextNodeInOrderFinder.findSuccessor(root, 7);
+
+        System.out.println(" Successor: " + successor);
+    }
+}
