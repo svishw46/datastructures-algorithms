3353b35ef5125d536f79bfd0b6ebc5826f3ff5b7
Rangaraju,Vengada Karthik <venkat060890@gmail.com>
2015-04-06 21:46:21 -0500

    Rename BSTBuilder to BSTInOrderBuilder

--- tree-algorithms/src/main/java/BSTInOrderBuilder.java
+++ tree-algorithms/src/main/java/BSTInOrderBuilder.java
@@ -4,22 +4,22 @@ import java.util.List;
 /**
  * Given a sorted array, write an algorithm to create a binary search tree with minimal height.
  */
-public class BSTBuilder {
+public class BSTInOrderBuilder {
     private List<Integer> sortedList;
 
-    public BSTBuilder(List<Integer> sortedList) {
+    public BSTInOrderBuilder(List<Integer> sortedList) {
         this.sortedList = sortedList;
     }
 
-    public Node build(int left, int right) {
+    public Node buildFromInOrder(int left, int right) {
         if (left > right) {//If cursors meet and start crossing in different directions.
             return null;
         }
 
         int middle = (left + right) / 2;
         Node root = new Node(this.sortedList.get(middle));
-        root.left = build(left, middle - 1);
-        root.right = build(middle + 1, right);
+        root.left = buildFromInOrder(left, middle - 1);
+        root.right = buildFromInOrder(middle + 1, right);
 
         return root;
     }
@@ -34,10 +34,10 @@ public class BSTBuilder {
         sortedList.add(6);
         sortedList.add(7);
 
-        BSTBuilder bstBuilder = new BSTBuilder(sortedList);
+        BSTInOrderBuilder bstInOrderBuilder = new BSTInOrderBuilder(sortedList);
         int initialLeft = 0;
         int initialRight = sortedList.size() - 1;
-        Node root = bstBuilder.build(initialLeft, initialRight);
+        Node root = bstInOrderBuilder.buildFromInOrder(initialLeft, initialRight);
 
         //In-order traversal of a BST should print in sorted order again.
         RecursiveTreeTraverser recursiveTreeTraverser = new RecursiveTreeTraverser();
